e51e58aed0c2c7b3c7a8fe66f4c01ad66f5f9a9a
User <User@DESKTOP-KFTDPI6>
2024-02-25 09:51:02 -0300

    Arquivos de configuração do projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+-- Para teste de integração com banco de dados sobre a operação de alteração de produto
+INSERT INTO PRODUTO (ID, NOME, CATEGORIA, PRECO_BASE, PRECO_TARIFADO) VALUES ('6f0d84c1-b6d2-4ec4-9a59-59f4caa22c09','Seguro de Vida Individual','VIDA',100,103.20);
